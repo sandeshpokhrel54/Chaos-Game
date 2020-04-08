6b619a534c63339dfec359ea2ab863a568133cfa
Sandesh Pokhrel <47052428+sandeshpokhrel54@users.noreply.github.com>
2020-04-08 22:10:35 +0545

    Added animation(the points are plotted live)

--- chaosGame(Serpenski'sGasket).py
+++ chaosGame(Serpenski'sGasket).py
@@ -1,55 +1,41 @@
 import matplotlib.pyplot as plt
 import random
+from matplotlib.animation import FuncAnimation
+
 
 plt.plot([0,1,2,0],[0,1,0,0])
 mx = 1.0
 my = 0.5
 
 
-for i in range(3000):
+def animate(i):
 
     
     a = random.randint(1,3)
+    global mx
+    global my
 
     if (a== 1):
-
         nmx = (mx+0)/2
-        nmy = (my+0)/2 
-        plt.plot(nmx,nmy,'go')
+        nmy = (my+0)/2        
         mx = nmx
         my = nmy
-
-
     elif (a== 2):
         nmx = (mx+1)/2
-        nmy = (my+1)/2 
-        plt.plot(nmx,nmy,'go')
+        nmy = (my+1)/2       
         mx = nmx
         my = nmy
-
-
     else :
         nmx = (mx+2)/2
-        nmy = (my+0)/2 
-        plt.plot(nmx,nmy,'go')
+        nmy = (my+0)/2         
         mx = nmx
         my = nmy
-        
-
-    
+            
     plt.plot(mx,my,'go')
     print(i)
     
 
+ani = FuncAnimation(plt.gcf(),animate,interval = 10)  #how often you want the function to be called interval in milliseconds
 plt.show()
 
-
-
-    
-
-
-    
-
-
-plt.show()
 
