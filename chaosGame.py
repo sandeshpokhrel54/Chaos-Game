f14704d565bfe9d052800044e53db363e2145ed5
Sandesh Pokhrel <47052428+sandeshpokhrel54@users.noreply.github.com>
2020-04-11 11:25:33 +0545

    Update chaosGame.py

--- chaosGame.py
+++ chaosGame.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import random
 
 
-n = int(input('Enter number of sides of the shape you want to create a gasket for i.e. 3 or 4 :'))
+n = int(input('Enter number of sides of the shape you want to create a gasket for i.e. 3,4 or 5 : '))
 
 if (n == 3): 
     plt.plot([0,1,2,0],[0,1,0,0])
@@ -48,76 +48,99 @@ if (n==4):
     plt.title("Chaos game with square")
     mx = 0.5
     my = 0.5
-    bouncer1 = True
-    bouncer2 = True
-    bouncer3 = True
-    bouncer4 = True
+    ran = 5
 
     for i in range(10000):
         
         a = random.randint(1,4)
 
-        if (a== 1 and bouncer1):
+        if (a== 1 and ran != a):
             
             nmx = (mx+0)/2
             nmy = (my+0)/2 
-            plt.plot(nmx,nmy,'go',markersize = 0.4)
             mx = nmx
             my = nmy
-            bouncer1 = False
-            bouncer2 = True
-            bouncer3 = True
-            bouncer4 = True
+            ran = a
             
            
 
-        elif (a== 2 and bouncer2):
+        elif (a== 2 and ran != a):
             nmx = (mx+1)/2
             nmy = (my+0)/2 
-            plt.plot(nmx,nmy,'go',markersize = 0.4)
             mx = nmx
             my = nmy
-            bouncer1 = True
-            bouncer2 = False
-            bouncer3 = True
-            bouncer4 = True
+            ran = a
             
               
-        elif (a== 3 and bouncer3):
+        elif (a== 3 and ran!= a):
             nmx = (mx+1)/2
             nmy = (my+1)/2 
-            plt.plot(nmx,nmy,'go',markersize = 0.4)
             mx = nmx
             my = nmy
-            bouncer1 = True
-            bouncer2 = True
-            bouncer3 = False
-            bouncer4 = True
+            ran = a
             
         
         elif (a== 4 and bouncer4):
             nmx = (mx+0)/2
             nmy = (my+1)/2 
-            plt.plot(nmx,nmy,'go',markersize = 0.4)
             mx = nmx
             my = nmy
-            bouncer1 = True
-            bouncer2 = True
-            bouncer3 = True
-            bouncer4 = False
+            ran = a
            
-
+        plt.show(mx,my,'go',markersize = 0.4)
         print(i) 
+        
+if (n == 5):
+    plt.plot([0,-0.61,-0.5,0.5,0.61,0],[1.22,0.61,0,0,0.61,1.22])
+    plt.title("Chaos game with pentagon")
+    mx = 0.5
+    my = 0.5
+    ran = 6
 
+    for i in range(50000):
+        
+        a = random.randint(1,5)
+
+        if (a== 1 and ran != a):
+            
+            nmx = (mx+0)/2
+            nmy = (my+1.22)/2 
+            mx = nmx
+            my = nmy
+            ran = a
+            
+        elif (a== 2 and ran !=a):
+            nmx = (mx-0.61)/2
+            nmy = (my+0.61)/2 
+            mx = nmx
+            my = nmy
+            ran = a 
+                        
+        elif (a== 3 and ran !=a):
+            nmx = (mx-0.5)/2
+            nmy = (my+0)/2 
+            mx = nmx
+            my = nmy
+            ran = a
+ 
+        elif (a== 4 and ran != a):
+            nmx = (mx+0.5)/2
+            nmy = (my+0)/2 
+            mx = nmx
+            my = nmy
+            ran = a
+        
+        elif(a==5 and ran!= a):
+            nmx = (mx+0.61)/2
+            nmy = (my+0.61)/2 
+            mx = nmx
+            my = nmy
+            ran = a
+
+            
+        print(i) 
+        plt.plot(mx,my,'go',markersize = 0.4)
+        
+        
+        
 plt.show()
-
-
-
-    
-
-
-    
-
-
-
-
